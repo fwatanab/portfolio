5b33bce55420368b53ba6f4bac6bd8170169df32
fwatanab <fwatanab@student.42tokyo.jp>
2025-04-02 13:43:08 +0900

    cssの基盤を大幅修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/app.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
